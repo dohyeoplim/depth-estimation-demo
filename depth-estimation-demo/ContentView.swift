fa9b3cbdc9ca8714312f46f6a8a85feaf04cc612
Dohyeop Lim <dhlim0817@gmail.com>
2025-07-17 18:45:55 +0900

    Initial Commit

---
+++ depth-estimation-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  depth-estimation-demo
+//
+//  Created by Dohyeop Lim on 2025-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
